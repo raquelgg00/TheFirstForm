d8af7df7fdc430934b575aceb97c4beabfa83443
raquelgg00 <raquelgarciaguillem@gmail.com>
2021-07-08 21:37:24 +0200

    cpp

--- src/pro/Juego/main.cpp
+++ src/pro/Juego/main.cpp
@@ -1,5 +1,5 @@
 #include "Partida.h"
-
+#include "ej_modulos/mysql/mysql.h"
 
 /*
 para usar valgrind primero instalarlo desde terminal y colocarse en la carpeta build de juego para ejecutar el comando:
@@ -7,6 +7,7 @@ valgrind --tool=memcheck --track-origins=yes ./MiJuego
 hay que fijarse solo en los valores de definitly lost que deben ser 0 y 0
 */
 int main(){
+  MYSQL* connection;
   Partida::Instance();
   return 0;
 }
