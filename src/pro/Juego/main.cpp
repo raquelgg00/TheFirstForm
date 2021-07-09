5433b78e2cd32edc2c9315c5b19e818acffbb14b
Gaspar Mora Rico <gmr31@gcloud.ua.es>
2021-07-09 17:05:17 +0200

    main del prototipo

--- src/pro/Juego/main.cpp
+++ src/pro/Juego/main.cpp
@@ -10,26 +10,28 @@ int main(){
 
   // Conexion BD
   MYSQL* conectar = mysql_init(0);
-  conectar = mysql_real_connect(conectar, "localhost", "id17210918_gasparmr6", "</IPvRwRAz~(OHh2", "id17210918_bd_webhost", 21, NULL, 0);
+  conectar = mysql_real_connect(conectar, "sql11.freesqldatabase.com", "sql11423929", "YJhMdyzuXx", "sql11423929", 3306, NULL, 0);
 
   MYSQL_ROW fila;
   MYSQL_RES* resultado;
 
   if(conectar){
-    string consulta = "select *";
+    cout<<"Conexión establecida con la BD\n";
+    string consulta = "select * from usuario";
     const char* c = consulta.c_str();
     int q_estado;
 
     q_estado = mysql_query(conectar, c);
     if(!q_estado){
+
       resultado = mysql_store_result(conectar);
       while(fila = mysql_fetch_row(resultado)){
-        cout<<fila[0]<<" , "<<fila[1];
+        cout<<fila[0]<<" --> "<<fila[1]<<std::endl;
       }
     }
   }
   else {
-    cout<<"No se conecto con la BD";
+    cout<<"No se pudo conectar con la BD\n";
   }
 
 
